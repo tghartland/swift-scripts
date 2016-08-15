eac40dc1afc7fc6923120a52cdffcd4fa7c6b1b1
Thomas Hartland <h4rtland@gmail.com>
2016-08-15 15:47:03 +0100

    added no username warning in settings.py

--- delete/settings.py
+++ delete/settings.py
@@ -1,18 +1,21 @@
 import getpass
 import os
+import sys
 
 # Auth settings
 # Looks for OpenStack credentials in environment
 # Defaults to second option if not found in environment
 
 username = os.getenv("OS_USERNAME") or ""
+if username == "":
+    print("No username specified in settings.py or user environment")
+    sys.exit()
 password = os.getenv("OS_PASSWORD") or getpass.getpass("OpenStack password (%s): " % username)
 auth_url = os.getenv("OS_AUTH_URL") or ""
 tenant_name = os.getenv("OS_TENANT_NAME") or ""
 
 if "OS_USERNAME" in os.environ:
     print("Using account '%s' from environment" % username)
-
 
 # Test settings
 
